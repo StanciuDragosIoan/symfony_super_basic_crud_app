c46f4bf4cfb0cf69bdf57d1355d2870d1dfd7bf2
Stanciu Dragos Ioan <stanciudragosioan92@gmail.com>
2020-01-09 23:20:43 +0200

    test

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+use App\Entity\Resource;
+use Doctrine\ORM\EntityManagerInterface;
+
+use Symfony\Component\HttpFoundation\Request;
+
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="default")
+     */
+    public function home()
+    {
+        return $this->render('default/index.html.twig', [
+            'title' => 'homepage',
+        ]);
+
+        
+    }
+
+    /**
+     * @Route("/add-resource", name="add_resource")
+     */
+    public function add_resource()
+    {   
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $resource = new Resource();
+        $resource->setValue('Sample Value');
+        $resource->setNumber(rand('1', '200'));
+        
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($resource);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return $this->render('default/add.html.twig', [
+            'title' => 'add resource',
+        ]);
+
+        
+    }
+
+    /**
+     * @Route("/database", name="database")
+     */
+    public function database()
+    {   
+        $repository = $this->getDoctrine()->getRepository(Resource::class);
+        $resources = $repository->findAll();
+
+
+        return $this->render('default/database.html.twig', [
+            'title' => 'database',
+            'resources' => $resources
+        ]);
+
+        
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete")
+     */
+    public function delete(Request $request)
+    {   
+         
+        $repository = $this->getDoctrine()->getRepository(Resource::class);
+
+        
+        $id = $request->attributes->get('id');
+         
+        
+  
+       
+        $resource =  $repository->find($id);
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $entityManager->remove($resource);
+        $entityManager->flush();
+
+        return $this->render('default/delete.html.twig', [
+            'title' => 'delete', 
+            'id' => $id
+        ]);
+
+        
+    }
+
+    public function getId(){
+
+        $id = 120;
+
+        return $id;
+    }
+}
---
+++ src/Entity/Resource.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ResourceRepository")
+ */
+class Resource
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $value;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $number;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getValue(): ?string
+    {
+        return $this->value;
+    }
+
+    public function setValue(string $value): self
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    public function getNumber(): ?int
+    {
+        return $this->number;
+    }
+
+    public function setNumber(int $number): self
+    {
+        $this->number = $number;
+
+        return $this;
+    }
+}
